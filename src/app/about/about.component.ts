8d7155d5c6a7a7fc5a325aa0b95028e4da161974
tarekyahia98 <eng.tarek.yahiia@gmail.com>
2024-12-23 14:52:40 +0200

    feat: add animations to about & contact components

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,13 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrls: ['./about.component.css'],
+  animations: [
+    trigger('slideInRight', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)', opacity: 0 }),
+        animate('3s ease-out', style({ transform: 'translateX(0)', opacity: 1 }))
+      ])
+    ])
+  ]
 })
-export class AboutComponent {
-
-}
+export class AboutComponent {}
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,14 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
+import { trigger, transition, style, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [CommonModule, MatIcon],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
+  animations: [
+    trigger('zoomIn', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'scale(0.8)' }),  
+        animate('1s ease-out', style({ opacity: 1, transform: 'scale(1)' }))
+      ])
+    ])
+  ]
 })
-export class ContactComponent {
-
-}
+export class ContactComponent {}
