84395199dfa232c40dcfa554f7f44942a2bc24fb
tarekyahia98 <eng.tarek.yahiia@gmail.com>
2024-12-23 10:52:54 +0200

    feat: update about & contact components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { AddUserComponent } from './add-user/add-user.component';
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AddUserComponent, MatToolbarModule, MatIconModule],
+  imports: [RouterOutlet, AddUserComponent, MatToolbarModule, MatIconModule, AboutComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIcon],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
