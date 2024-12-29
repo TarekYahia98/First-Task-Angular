9ac24359c6d4f1314523e4ec3cf4b2fdccf1c28c
tarekyahia98 <eng.tarek.yahiia@gmail.com>
2024-12-29 14:10:57 +0200

    feat: add apis in user service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -8,6 +8,7 @@ import { User } from '../app/models/user.model';
 })
 export class UserService {
   private apiUrl = 'https://jsonplaceholder.typicode.com/users';
+  // private apiUrl = 'http://localhost:8080/api/v1/users';
 
   constructor(private http: HttpClient) {}
 
@@ -18,4 +19,17 @@ export class UserService {
   getUserById(id: number): Observable<User> {
     return this.http.get<User>(`${this.apiUrl}/${id}`);
   }
+
+  createUser(user: User): Observable<User> {
+    return this.http.post<User>(`${this.apiUrl}/addUser`, user);
+  }
+
+  updateUser(id: number, userDetails: User): Observable<User> {
+    return this.http.put<User>(`${this.apiUrl}/user/${id}`, userDetails);
+  }
+
+  deleteUser(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/user/${id}`);
+  }
 }
+
